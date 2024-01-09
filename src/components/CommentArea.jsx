048a583495103e7b318aa8c2229f05f970905acc
DanielSantini <danielsantini36@gmail.com>
2024-01-09 15:43:02 +0100

    ok

--- src/components/CommentArea.jsx
+++ src/components/CommentArea.jsx
@@ -1,39 +1,23 @@
-import { Component } from 'react'
-import CommentsList from './CommentsList'
-import AddComment from './AddComment'
+import React, { useEffect, useState } from 'react';
+import CommentsList from './CommentsList';
+import AddComment from './AddComment';
 
-class CommentArea extends Component {
-  state = {
-    comments: [],
-  }
+const CommentArea = ({ bookId }) => {
+  const [comments, setComments] = useState([]);
 
-  // lo commento perchè all'avvio non ci sarà mai più bisogno di invocare getComments!
-  // lo devo fare ora OGNI VOLTA che cambio il libro selezionato...
-  // componentDidMount = () => {
-  //   // viene eseguito una volta sola, all'avvio del componente!
-  //   // ora faremo la fetch per recuperare i commenti
-  //   if (this.props.bookId) {
-  //     this.getComments()
-  //   }
-  // }
-
-  componentDidUpdate(prevProps, prevState) {
-    // dobbiamo riconoscere ogni cambio di asin selezionato
-    if (prevProps.bookId !== this.props.bookId) {
-      // è cambiato il bookId, quindi il libro selezionato!
-      // qua facciamo la fetch
-      this.getComments()
+  useEffect(() => {
+    if (bookId) {
+      getComments();
     }
-  }
+  }, [bookId]);
 
-  aggiornaCommenti = () => {
-    this.getComments()
-  }
+  const aggiornaCommenti = () => {
+    getComments();
+  };
 
-  getComments = () => {
+  const getComments = () => {
     fetch(
-      'https://striveschool-api.herokuapp.com/api/comments/' +
-        this.props.bookId,
+      'https://striveschool-api.herokuapp.com/api/comments/' + bookId,
       {
         headers: {
           Authorization:
@@ -43,34 +27,30 @@ class CommentArea extends Component {
     )
       .then((res) => {
         if (res.ok) {
-          return res.json()
+          return res.json();
         } else {
-          throw new Error('errore nel recupero dei commenti')
+          throw new Error('errore nel recupero dei commenti');
         }
       })
       .then((arrayOfComments) => {
-        console.log(arrayOfComments)
-        this.setState({
-          comments: arrayOfComments,
-        })
+        console.log(arrayOfComments);
+        setComments(arrayOfComments);
       })
       .catch((err) => {
-        console.log('error', err)
-      })
-  }
+        console.log('error', err);
+      });
+  };
 
-  render() {
-    return (
+  return (
+    <div>
       <div>
-        <div>
-          <CommentsList reviews={this.state.comments} />
-        </div>
-        <div>
-          <AddComment bookId={this.props.bookId} aggiornaCommenti={this.aggiornaCommenti} />
-        </div>
+        <CommentsList reviews={comments} />
       </div>
-    )
-  }
-}
+      <div>
+        <AddComment bookId={bookId} aggiornaCommenti={aggiornaCommenti} />
+      </div>
+    </div>
+  );
+};
 
-export default CommentArea
+export default CommentArea;
